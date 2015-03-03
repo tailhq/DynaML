6142b825155c594b1e3f1e6524b8808d576ee2c7
mandar2812 <mandar2812@gmail.com>
2015-03-03 18:42:49 +0100

    Added subset selection based on Renyi Entropy, Silvermans rule of thumb for bandwidth selection

--- src/main/scala/org/kuleuven/esat/utils/package.scala
+++ src/main/scala/org/kuleuven/esat/utils/package.scala
@@ -1,6 +1,7 @@
 package org.kuleuven.esat
 
 import java.io.File
+import breeze.linalg.DenseVector
 import com.github.tototoshi.csv.{QUOTE_NONNUMERIC, DefaultCSVFormat, CSVReader}
 
 /**
@@ -25,5 +26,35 @@ package object utils {
       override val quoting = QUOTE_NONNUMERIC
     }
     CSVReader.open(new File(file))
+  }
+
+  /**
+   * Get the mean and variance of a data set
+   * which is a [[List]] of [[DenseVector]].
+   *
+   * @param data The data set.
+   *
+   * @return A [[Tuple2]] containing the mean
+   *         and variance * n-1.
+   *
+   * */
+  def getStats(data: List[DenseVector[Double]]):
+  (DenseVector[Double], DenseVector[Double]) = {
+    def getStatsRec(d: List[DenseVector[Double]],
+                    m: DenseVector[Double],
+                    s: DenseVector[Double],
+                    i: Int):
+    (DenseVector[Double], DenseVector[Double]) = d match {
+      case Nil => (m, s)
+      case x :: rest => {
+        getStatsRec(rest, m + (x - m):/=i.toDouble,
+          s + ((x - m) :* (x - (m + (x - m):/=i.toDouble))),
+          i - 1)
+      }
+    }
+
+    getStatsRec(data.tail, data.head,
+      DenseVector.zeros[Double](data.head.length),
+      data.length)
   }
 }
