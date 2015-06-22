8a58a31fa805cb97fe6f39d15d98b4fbd94e304f
mandar2812 <mandar2812@gmail.com>
2015-06-22 12:06:23 +0200

    Add args to testMagicGamma

--- testMagicGamma.scala
+++ testMagicGamma.scala
@@ -14,7 +14,7 @@ val configtest = Map("file" -> "data/magicgammatest.csv",
   "delim" -> ",",
   "head" -> "false")
 
-val conf = new SparkConf().setAppName("Magicgamma").setMaster("local[4]")
+val conf = new SparkConf().setAppName("Magicgamma").setMaster("local["+args(0).toInt+"]")
 
 conf.registerKryoClasses(Array(classOf[LSSVMSparkModel]))
 
@@ -22,7 +22,7 @@ val sc = new SparkContext(conf)
 
 val model = LSSVMSparkModel(config, sc)
 
-model.applyKernel(new RBFKernel(120.0), 150)
+model.applyKernel(new RBFKernel(100.0))
 model.setRegParam(1.5).setLearningRate(0.001).setMaxIterations(5).learn()
 
 val met = model.evaluate(configtest)
