dc081db0f1715933ce7b858450080823a3336002
mandar2812 <mandar2812@gmail.com>
2015-07-01 16:56:21 +0200

    Preprocess SUSY

--- PreprocessSusy.scala
+++ PreprocessSusy.scala
@@ -3,6 +3,10 @@ import java.io.InputStreamReader
 import java.util.zip.GZIPInputStream
 import java.io.FileInputStream
 
+import com.github.tototoshi.csv.CSVWriter
+
+import scala.util.Random
+
 case class BufferedReaderIterator(reader: BufferedReader) extends Iterator[String] {
   override def hasNext() = reader.ready
   override def next() = reader.readLine()
@@ -19,10 +23,24 @@ object GzFileIterator {
 
 object PreprocessSusy {
   def apply(args: String = "") = {
-    val iterator:BufferedReader = GzFileIterator(new java.io.File("data/SUSY.csv.gz"), "US-ASCII")
-    (1 to args.toInt).foreach((_) =>println(iterator.readLine()))
-    
+    val iterator:BufferedReader = GzFileIterator(new java.io.File(args+"SUSY.csv.gz"),
+      "US-ASCII")
+    var line = iterator.readLine()
+    val writer = CSVWriter.open(args+"susy.csv")
+    val writert = CSVWriter.open(args+"susytest.csv")
 
+    println("Outputting train and test csv files ...")
+    while(line.toBoolean) {
+      if(Random.nextDouble() <= 0.9)
+      {
+        writer.writeRow(line.split(',').reverse)
+      } else {
+        writert.writeRow(line.split(',').reverse)
+      }
+      line = iterator.readLine()
+    }
+    writer.close()
+    writert.close()
+    println("Done ...")
   }
-
 }
