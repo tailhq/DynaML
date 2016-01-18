d58e289da9a604a2f2ca437b2b64667f31ca5fff
mandar2812 <mandar2812@gmail.com>
2016-01-18 23:12:42 +0100

    Changes to GP Housing example: ML-II and Composite Kernels

--- src/main/scala/io/github/mandar2812/dynaml/examples/TestGPHousing.scala
+++ src/main/scala/io/github/mandar2812/dynaml/examples/TestGPHousing.scala
@@ -4,6 +4,7 @@ import breeze.linalg.{DenseMatrix, DenseVector}
 import io.github.mandar2812.dynaml.evaluation.RegressionMetrics
 import io.github.mandar2812.dynaml.kernels._
 import io.github.mandar2812.dynaml.models.gp.GPRegression
+import io.github.mandar2812.dynaml.optimization.{GPMLOptimizer, GridSearch}
 import io.github.mandar2812.dynaml.pipes.{StreamDataPipe, DataPipe}
 import io.github.mandar2812.dynaml.utils
 
@@ -11,10 +12,28 @@ import io.github.mandar2812.dynaml.utils
   * Created by mandar on 15/12/15.
   */
 object TestGPHousing {
-  def apply (kern: String = "RBF",
-             bandwidth: Double = 0.5, noise: Double = 0.0,
-             trainFraction: Double = 0.75,
-             columns: List[Int] = List(13,0,1,2,3,4,5,6,7,8,9,10,11,12)): Unit = {
+
+  def apply(kernel: CovarianceFunction[DenseVector[Double], Double, DenseMatrix[Double]],
+            bandwidth: Double = 0.5, noise: Double = 0.0,
+            trainFraction: Double = 0.75,
+            columns: List[Int] = List(13,0,1,2,3,4,5,6,7,8,9,10,11,12),
+            grid: Int = 5, step: Double = 0.2, globalOpt: String = "ML",
+            stepSize: Double = 0.01, maxIt: Int = 300): Unit =
+    runExperiment(kernel, bandwidth,
+      noise, (506*trainFraction).toInt, columns,
+      grid, step, globalOpt,
+      Map("tolerance" -> "0.0001",
+        "step" -> stepSize.toString,
+        "maxIterations" -> maxIt.toString
+      )
+    )
+
+  def apply(kern: String,
+            bandwidth: Double, noise: Double,
+            trainFraction: Double,
+            columns: List[Int],
+            grid: Int, step: Double, globalOpt: String,
+            stepSize: Double, maxIt: Int): Unit = {
 
     val kernel: CovarianceFunction[DenseVector[Double], Double, DenseMatrix[Double]] =
       kern match {
@@ -28,14 +47,34 @@ object TestGPHousing {
           new RationalQuadraticKernel(bandwidth)
         case "FBM" => new FBMKernel(bandwidth)
         case "Student" => new TStudentKernel(bandwidth)
+        case "Periodic" => new PeriodicKernel(bandwidth, bandwidth)
       }
+
+    val num_training = 506*trainFraction
+
+    runExperiment(kernel, bandwidth,
+      noise, num_training.toInt, columns,
+      grid, step, globalOpt,
+      Map("tolerance" -> "0.0001",
+        "step" -> stepSize.toString,
+        "maxIterations" -> maxIt.toString
+      )
+    )
+
+  }
+
+  def runExperiment(kernel: CovarianceFunction[DenseVector[Double], Double, DenseMatrix[Double]],
+                    bandwidth: Double = 0.5, noise: Double = 0.0,
+                    num_training: Int = 200, columns: List[Int] = List(40,16,21,23,24,22,25),
+                    grid: Int = 5, step: Double = 0.2,
+                    globalOpt: String = "ML", opt: Map[String, String]): Unit = {
 
     //Load Housing data into a stream
     //Extract the time and Dst values
     //separate data into training and test
     //pipe training data to model and then generate test predictions
     //create RegressionMetrics instance and produce plots
-    val num_training = 506*trainFraction
+
 
     val replaceWhiteSpaces = (s: Stream[String]) => s.map(utils.replace("\\s+")(","))
 
@@ -69,6 +108,23 @@ object TestGPHousing {
         Stream[(DenseVector[Double], Double)]),
         (DenseVector[Double], DenseVector[Double]))) => {
         val model = new GPRegression(kernel, trainTest._1._1.toSeq).setNoiseLevel(noise)
+
+        val gs = globalOpt match {
+          case "GS" => new GridSearch[model.type](model)
+            .setGridSize(grid)
+            .setStepSize(step)
+            .setLogScale(false)
+
+          case "ML" => new GPMLOptimizer[DenseVector[Double],
+            Seq[(DenseVector[Double], Double)],
+            GPRegression](model)
+        }
+
+        val startConf = kernel.state ++ Map("noiseLevel" -> noise)
+        val (_, conf) = gs.optimize(kernel.state + ("noiseLevel" -> noise), opt)
+
+        model.setState(conf)
+
         val res = model.test(trainTest._1._2.toSeq)
         val scoresAndLabelsPipe =
           DataPipe(
