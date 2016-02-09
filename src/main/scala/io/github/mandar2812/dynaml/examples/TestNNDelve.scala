a30a9b108041a6ce3018a8f97c77a591adf2ebae
mandar2812 <mandar2812@gmail.com>
2016-02-09 07:36:13 +0100

    Added Test for FF Network on Fried Delve data

---
+++ src/main/scala/io/github/mandar2812/dynaml/examples/TestNNDelve.scala
@@ -0,0 +1,108 @@
+package io.github.mandar2812.dynaml.examples
+
+import breeze.linalg.{DenseMatrix, DenseVector}
+import io.github.mandar2812.dynaml.evaluation.RegressionMetrics
+import io.github.mandar2812.dynaml.kernels._
+import io.github.mandar2812.dynaml.models.gp.GPRegression
+import io.github.mandar2812.dynaml.models.neuralnets.{FeedForwardNetwork, FFNeuralGraph}
+import io.github.mandar2812.dynaml.pipes.{StreamDataPipe, DataPipe}
+import io.github.mandar2812.dynaml.utils
+
+/**
+  * Created by mandar on 11/1/16.
+  */
+object TestNNDelve {
+  def apply (hidden: Int = 2, nCounts:List[Int] = List(), acts:List[String],
+             training: Int = 100, test: Int = 1000,
+             columns: List[Int] = List(10,0,1,2,3,4,5,6,7,8,9),
+             stepSize: Double = 0.01, maxIt: Int = 30, mini: Double = 1.0,
+             alpha: Double = 0.5): Unit = {
+
+    //Load Housing data into a stream
+    //Extract the time and Dst values
+    //separate data into training and test
+    //pipe training data to model and then generate test predictions
+    //create RegressionMetrics instance and produce plots
+
+    val extractTrainingFeatures = (l: Stream[String]) =>
+      utils.extractColumns(l, ",", columns, Map())
+
+    val normalizeData =
+      (trainTest: (Stream[(DenseVector[Double], Double)], Stream[(DenseVector[Double], Double)])) => {
+
+        val (mean, variance) = utils.getStats(trainTest._1.map(tup =>
+          DenseVector(tup._1.toArray ++ Array(tup._2))).toList)
+
+        val stdDev: DenseVector[Double] = variance.map(v =>
+          math.sqrt(v/(trainTest._1.length.toDouble - 1.0)))
+
+
+        val normalizationFunc = (point: (DenseVector[Double], Double)) => {
+          val extendedpoint = DenseVector(point._1.toArray ++ Array(point._2))
+
+          val normPoint = (extendedpoint - mean) :/ stdDev
+          val length = normPoint.length
+          (normPoint(0 until length), normPoint(-1))
+        }
+
+        ((trainTest._1.map(normalizationFunc),
+          trainTest._2.map(normalizationFunc)), (mean, stdDev))
+      }
+
+    val modelTrainTest =
+      (trainTest: ((Stream[(DenseVector[Double], Double)],
+        Stream[(DenseVector[Double], Double)]),
+        (DenseVector[Double], DenseVector[Double]))) => {
+
+        val gr = FFNeuralGraph(trainTest._1._1.head._1.length, 1, hidden,
+          acts, nCounts)
+
+        val transform = DataPipe((d: Stream[(DenseVector[Double], Double)]) =>
+          d.map(el => (el._1, DenseVector(el._2))))
+
+        val model = new FeedForwardNetwork[Stream[(DenseVector[Double], Double)]](trainTest._1._1, gr, transform)
+
+        model.setLearningRate(stepSize)
+          .setMaxIterations(maxIt)
+          .setBatchFraction(mini)
+          .setMomentum(alpha)
+          .learn()
+
+        val res = model.test(trainTest._1._2)
+        val scoresAndLabelsPipe =
+          DataPipe(
+            (res: Seq[(DenseVector[Double], DenseVector[Double])]) =>
+              res.map(i => (i._1(0), i._2(0))).toList) > DataPipe((list: List[(Double, Double)]) =>
+            list.map{l => (l._1*trainTest._2._2(-1) + trainTest._2._1(-1),
+              l._2*trainTest._2._2(-1) + trainTest._2._1(-1))})
+
+        val scoresAndLabels = scoresAndLabelsPipe.run(res)
+
+        val metrics = new RegressionMetrics(scoresAndLabels,
+          scoresAndLabels.length)
+
+        metrics.print()
+        metrics.generatePlots()
+      }
+
+    val preProcessPipe = DataPipe(utils.textFileToStream _) >
+      DataPipe(extractTrainingFeatures) >
+      StreamDataPipe((line: String) => {
+        val split = line.split(",")
+        (DenseVector(split.tail.map(_.toDouble)), split.head.toDouble)
+      })
+
+    val trainTestPipe = DataPipe(preProcessPipe, preProcessPipe) >
+      DataPipe((data: (Stream[(DenseVector[Double], Double)],
+        Stream[(DenseVector[Double], Double)])) => {
+        (data._1.take(training),
+          data._2.takeRight(test))
+      }) >
+      DataPipe(normalizeData) >
+      DataPipe(modelTrainTest)
+
+    trainTestPipe.run(("data/delve.csv", "data/delve.csv"))
+
+  }
+
+}
