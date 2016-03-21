5c3f6716da4ddd16c49594ccb5118ed76143efa3
mandar2812 <mandar2812@gmail.com>
2016-03-21 13:30:16 +0100

    Adding License comment to head of source files

--- src/main/scala/io/github/mandar2812/dynaml/examples/TestOmniARX.scala
+++ src/main/scala/io/github/mandar2812/dynaml/examples/TestOmniARX.scala
@@ -1,3 +1,21 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+* */
 package io.github.mandar2812.dynaml.examples
 
 import java.io.File
@@ -42,7 +60,8 @@ object TestOmniARX {
       grid, step, globalOpt,
       Map("tolerance" -> "0.0001",
         "step" -> stepSize.toString,
-        "maxIterations" -> maxIt.toString),
+        "maxIterations" -> maxIt.toString,
+        "Use VBz" -> "false"),
       action)
 
   def runExperiment(trainstart: String, trainend: String,
@@ -158,7 +177,6 @@ object TestOmniARX {
         metrics.setName(name)
 
         metrics.print()
-        //metrics.generateFitPlot()
 
         //Plotting time series prediction comparisons
         line((1 to scoresAndLabels.length).toList, scoresAndLabels.map(_._2))
@@ -279,7 +297,8 @@ object DstARXExperiment {
               grid, step, "GS",
               Map("tolerance" -> "0.0001",
                 "step" -> "0.1",
-                "maxIterations" -> "100"))
+                "maxIterations" -> "100",
+                "Use VBz" -> "false"))
               .foreach(res => writer.writeRow(res))
           })
 
--- src/main/scala/io/github/mandar2812/dynaml/models/svm/KernelLSSVMModel.scala
+++ src/main/scala/io/github/mandar2812/dynaml/models/svm/KernelLSSVMModel.scala
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.github.mandar2812.dynaml.models.svm
 
 import breeze.linalg.{DenseMatrix, DenseVector, norm}
