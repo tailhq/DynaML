17e65e758c1d783c8f0a0f2fc086e287a7f9cd89
mandar2812 <mandar2812@gmail.com>
2016-04-10 01:32:40 +0200

    [WIP]: TL Omni model test

--- src/main/scala/io/github/mandar2812/dynaml/examples/TestOmniNarmax.scala
+++ src/main/scala/io/github/mandar2812/dynaml/examples/TestOmniNarmax.scala
@@ -27,21 +27,7 @@ object TestOmniNarmax {
   def apply(start: String = "2006/12/28/00",
             end: String = "2006/12/29/23"): Seq[Seq[Double]] = {
 
-    //Load Omni data into a stream
-    //Extract the time and Dst values
-    //separate data into training and test
-    //pipe training data to model and then generate test predictions
-    //create RegressionMetrics instance and produce plots
     val logger = Logger.getLogger(this.getClass)
-    val replaceWhiteSpaces = (s: Stream[String]) => s.map(utils.replace("\\s+")(","))
-
-    val extractTrainingFeatures = (l: Stream[String]) =>
-      utils.extractColumns(l, ",", List(0,1,2,40),
-        Map(16 -> "999.9", 21 -> "999.9",
-          24 -> "9999.", 23 -> "999.9",
-          40 -> "99999", 22 -> "9999999.",
-          25 -> "999.9", 28 -> "99.99",
-          27 -> "9.999", 39 -> "999"))
 
     val names = Map(
       24 -> "Solar Wind Speed",
@@ -172,6 +158,127 @@ object TestOmniNarmax {
     preProcessPipe.run("data/omni2_"+yearTest+".csv")
   }
 }
+
+
+object TestOmniTL {
+
+  val logger = Logger.getLogger(this.getClass)
+
+  def apply(start: String = "2006/12/28/00",
+            end: String = "2006/12/29/23") = {
+
+
+
+    val names = Map(
+      24 -> "Solar Wind Speed",
+      16 -> "I.M.F Bz",
+      40 -> "Dst",
+      41 -> "AE",
+      38 -> "Kp",
+      39 -> "Sunspot Number",
+      28 -> "Plasma Flow Pressure",
+      23 -> "Proton Density"
+    )
+
+    val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy/MM/dd/HH")
+    val dateS: Date = sdf.parse(start)
+    val dateE: Date = sdf.parse(end)
+
+    val greg: GregorianCalendar = new GregorianCalendar()
+    greg.setTime(dateS)
+    val dayStart = greg.get(Calendar.DAY_OF_YEAR)
+    val hourStart = greg.get(Calendar.HOUR_OF_DAY)
+    val stampStart = (dayStart * 24) + hourStart
+    val yearTest = greg.get(Calendar.YEAR)
+
+
+    val yearStart = greg.get(Calendar.YEAR).toString
+
+    val monthStart = if(greg.get(Calendar.MONTH) < 9) {
+      "0"+(greg.get(Calendar.MONTH)+1).toString
+    } else {
+      (greg.get(Calendar.MONTH)+1).toString
+    }
+
+    val fileNameS = "dst_"+yearStart+"_"+monthStart+".txt"
+
+
+
+
+    greg.setTime(dateE)
+    val dayEnd = greg.get(Calendar.DAY_OF_YEAR)
+    val hourEnd = greg.get(Calendar.HOUR_OF_DAY)
+    val stampEnd = (dayEnd * 24) + hourEnd
+
+
+
+
+
+
+
+  }
+
+  def prepareTLFiles() = {
+
+    val stormsPipe =
+      DynaMLPipe.fileToStream >
+        DynaMLPipe.replaceWhiteSpaces >
+        StreamDataPipe((stormEventData: String) => {
+          val stormMetaFields = stormEventData.split(',')
+
+          val startDate = stormMetaFields(1)
+          val endDate = stormMetaFields(3)
+
+          val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy/MM/dd")
+          val greg: GregorianCalendar = new GregorianCalendar()
+
+          greg.setTime(sdf.parse(startDate))
+          val yearStart = greg.get(Calendar.YEAR).toString
+
+          val monthStart = if(greg.get(Calendar.MONTH) < 9) {
+            "0"+(greg.get(Calendar.MONTH)+1).toString
+          } else {
+            (greg.get(Calendar.MONTH)+1).toString
+          }
+
+          val fileNameS = "dst_"+yearStart+"_"+monthStart+".txt"
+
+          greg.setTime(sdf.parse(endDate))
+
+          val yearEnd = greg.get(Calendar.YEAR).toString
+
+          val monthEnd = if(greg.get(Calendar.MONTH) < 9) {
+            "0"+(greg.get(Calendar.MONTH)+1).toString
+          } else {
+            (greg.get(Calendar.MONTH)+1).toString
+          }
+
+          val fileNameE = "dst_"+yearEnd+"_"+monthEnd+".txt"
+
+          if(fileNameE == fileNameS) {
+            logger.info("Same Month")
+            utils.downloadURL(
+              "http://lasp.colorado.edu/space_weather/dsttemerin/archive/"+fileNameS,
+              "data/"+fileNameS)
+          } else {
+            logger.info("Different Months!")
+            utils.downloadURL(
+              "http://lasp.colorado.edu/space_weather/dsttemerin/archive/"+fileNameS,
+              "data/"+fileNameS)
+
+            utils.downloadURL(
+              "http://lasp.colorado.edu/space_weather/dsttemerin/archive/"+fileNameE,
+              "data/"+fileNameE)
+          }
+
+        })
+
+    stormsPipe.run("data/geomagnetic_storms.csv")
+
+  }
+
+}
+
 
 object DstNMExperiment {
 
