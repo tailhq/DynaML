2ad10a21ba6b40a80c48f770aa748d435c108047
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2016-06-24 12:04:44 +0530

    Update generalDLM.scala
    Initialised a function which filters a sequence of data using foldLeft

--- dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/generalDLM.scala
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/generalDLM.scala
@@ -20,3 +20,31 @@ Data(t, x + Gaussian(0, sqrt(p.a)).draw, Some(x)) }
 val p = Parameters(3.0, 0.5, 0.0, 10.0)
 // simulate 16 different realisations of 100 observations, representing 16 stations
 val data = (1 to 16) map (id =&gt; (id, simulate(p).take(100).toVector))
+
+// Using properties of multivariate normal distribution
+// The equations required to program up the Kalman filter
+case class FilterOut(data: Data, p: Parameters)
+ 
+def filter(d: Data, p: Parameters): FilterOut = {
+// update the mean and variance of the posterior to determine the state space
+val e1 = d.observation - p.m0
+val a1 = (p.c0 + p.b)/(p.c0 + p.b + p.a)
+val m1 = p.m0 + a1 * e1
+val c1 = a1 * p.a
+ 
+// return the data with the expectation of the hidden state and the updated Parameters
+FilterOut(Data(d.time, d.observation, Some(m1)), Parameters(p.a, p.b, m1, c1))
+}
+def filterSeries(data: Seq[Data],initParams: Parameters): Seq[FilterOut] = {
+ 
+val (a, b) = (initParams.a, initParams.b) // a and b are fixed
+val initFilter = Vector[FilterOut](filter(data.head, initParams)) // initialise the filter
+ 
+data.tail.foldLeft(initFilter)((acc, nextObservation) =&gt; {
+// construct the parameters from the previous step of the filter
+val p = Parameters(a, b, acc.head.p.m0, acc.head.p.c0)
+ 
+// add the filtered observation to the head of the list
+filter(nextObservation, p) +: acc
+}).reverse
+}
