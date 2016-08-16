25d0f3f379f595e10e40816ba41b6ecd5fd550d2
mandar2812 <mandar2812@gmail.com>
2016-08-16 11:04:20 +0200

    Added multivariate gaussian scaler class

--- dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/utils/GaussianScaler.scala
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/utils/GaussianScaler.scala
@@ -1,10 +1,17 @@
 package io.github.mandar2812.dynaml.utils
 
-import breeze.linalg.DenseVector
+import breeze.linalg.{DenseMatrix, DenseVector, cholesky, inv}
 import io.github.mandar2812.dynaml.pipes.{ReversibleScaler, Scaler}
 
 /**
-  * Created by mandar on 17/6/16.
+  * @author mandar2812 date: 17/6/16.
+  *
+  * Scales attributes of a vector pattern using the sample mean and variance of
+  * each dimension. This assumes that there is no covariance between the data
+  * dimensions.
+  *
+  * @param mean Sample mean of the data
+  * @param sigma Sample variance of each data dimension
   */
 case class GaussianScaler(mean: DenseVector[Double], sigma: DenseVector[Double])
   extends ReversibleScaler[DenseVector[Double]]{
@@ -12,4 +19,24 @@ case class GaussianScaler(mean: DenseVector[Double], sigma: DenseVector[Double])
     Scaler((pattern: DenseVector[Double]) => (pattern :* sigma) + mean)
 
   override def run(data: DenseVector[Double]): DenseVector[Double] = (data-mean) :/ sigma
+}
+
+
+/**
+  * Scales the attributes of a data pattern using the sample mean and covariance matrix
+  * calculated on the data set. This allows standardization of multivariate data sets
+  * where the covariance of individual data dimensions is not negligible.
+  *
+  * @param mean Sample mean of data
+  * @param sigma Sample covariance matrix of data.
+  * */
+case class MVGaussianScaler(mean: DenseVector[Double], sigma: DenseMatrix[Double])
+  extends ReversibleScaler[DenseVector[Double]] {
+
+  val sigmaInverse = cholesky(inv(sigma))
+
+  override val i: Scaler[DenseVector[Double]] =
+    Scaler((pattern: DenseVector[Double]) => (inv(sigmaInverse.t) * pattern) + mean)
+
+  override def run(data: DenseVector[Double]): DenseVector[Double] = sigmaInverse.t * (data - mean)
 }
