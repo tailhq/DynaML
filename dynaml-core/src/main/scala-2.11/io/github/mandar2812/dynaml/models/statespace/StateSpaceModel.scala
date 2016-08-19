ebd80453bb1689c0384d1e62499f83e612a4f3a1
mandar2812 <mandar2812@gmail.com>
2016-08-19 11:17:22 +0200

    Indentation commit

--- dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/models/statespace/StateSpaceModel.scala
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/models/statespace/StateSpaceModel.scala
@@ -21,50 +21,52 @@ trait StateSpaceModel extends Serializable {
 }
 
 object StateSpaceModel {
-  def op(mod1: Parameters => StateSpaceModel, mod2: Parameters => StateSpaceModel): Parameters => StateSpaceModel = p => new StateSpaceModel {
+  def op(mod1: Parameters => StateSpaceModel,
+         mod2: Parameters => StateSpaceModel): Parameters => StateSpaceModel =
+    p => new StateSpaceModel {
 
-    def observation = x => p match {
-      case BranchParameter(lp,_) => mod1(lp).observation(x)
-      case param: LeafParameter => mod1(param).observation(x)
-    }
-
-    override def link(x: Double) = mod1(p).link(x)
-
-    def f(s: State, t: Time) = s match {
-      case BranchState(ls, rs) =>
-        mod1(p).f(ls, t) + mod2(p).f(rs, t)
-      case x: LeafState =>
-        mod1(p).f(x, t)
-    }
-
-    def x0 = p match {
-      case BranchParameter(lp, rp) =>
-        for {
-          l <- mod1(lp).x0
-          r <- mod2(rp).x0
-        } yield l |+| r
-      case param: LeafParameter =>
-        for {
-          l <- mod1(param).x0
-          r <- mod2(param).x0
-        } yield l |+| r
-    }
-
-    def stepFunction = (s, dt) => (s, p) match {
-      case (BranchState(ls, rs), BranchParameter(lp, rp)) =>
-        for {
-          l <- mod1(lp).stepFunction(ls, dt)
-          r <- mod2(rp).stepFunction(rs, dt)
-        } yield BranchState(l, r)
-      case (x: LeafState, param: LeafParameter) => // Null model case, non-null must be on left
-        mod1(param).stepFunction(x, dt)
-    }
-
-     def dataLikelihood = (s, y) => p match {
-      case param: LeafParameter => mod1(param).dataLikelihood(s, y)
-      case BranchParameter(lp, _) => mod1(lp).dataLikelihood(s, y)
+      def observation = x => p match {
+        case BranchParameter(lp,_) => mod1(lp).observation(x)
+        case param: LeafParameter => mod1(param).observation(x)
       }
-  }
+
+      override def link(x: Double) = mod1(p).link(x)
+
+      def f(s: State, t: Time) = s match {
+        case BranchState(ls, rs) =>
+          mod1(p).f(ls, t) + mod2(p).f(rs, t)
+        case x: LeafState =>
+          mod1(p).f(x, t)
+      }
+
+      def x0 = p match {
+        case BranchParameter(lp, rp) =>
+          for {
+            l <- mod1(lp).x0
+            r <- mod2(rp).x0
+          } yield l |+| r
+        case param: LeafParameter =>
+          for {
+            l <- mod1(param).x0
+            r <- mod2(param).x0
+          } yield l |+| r
+      }
+
+      def stepFunction = (s, dt) => (s, p) match {
+        case (BranchState(ls, rs), BranchParameter(lp, rp)) =>
+          for {
+            l <- mod1(lp).stepFunction(ls, dt)
+            r <- mod2(rp).stepFunction(rs, dt)
+          } yield BranchState(l, r)
+        case (x: LeafState, param: LeafParameter) => // Null model case, non-null must be on left
+          mod1(param).stepFunction(x, dt)
+      }
+
+      def dataLikelihood = (s, y) => p match {
+        case param: LeafParameter => mod1(param).dataLikelihood(s, y)
+        case BranchParameter(lp, _) => mod1(lp).dataLikelihood(s, y)
+      }
+    }
 
   def zeroModel(stepFun: SdeParameter => (State, TimeIncrement) => Rand[State]): Parameters => StateSpaceModel = p => new StateSpaceModel {
     def observation = x => new Rand[Observation] { def draw = x.head }
