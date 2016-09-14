5dacd031ed632ebd0b833961efbc11808e342da2
mandar2812 <mandar2812@gmail.com>
2016-09-14 11:35:35 +0100

    Added MLP kernel
    The MLP kernel/covariance function results from a Bayesian
    marginalisation of an infinite feature map constructed from sigmoid
    activated neurons in single layer feed forward nets.

---
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/kernels/MLPKernel.scala
@@ -0,0 +1,31 @@
+package io.github.mandar2812.dynaml.kernels
+
+import breeze.linalg.{DenseMatrix, DenseVector}
+
+/**
+  * @author mandar2812 date 13/09/16.
+  *
+  * Implementation of the Maximum Likelihood Perceptron (MLP) kernel
+  */
+class MLPKernel(w: Double, b: Double) extends SVMKernel[DenseMatrix[Double]]
+  with LocalSVMKernel[DenseVector[Double]]
+  with Serializable{
+
+  override val hyper_parameters = List("w", "b")
+
+  state = Map("w" -> w, "b" -> b)
+
+  def setw(d: Double): Unit = {
+    state += ("w" -> d.toDouble)
+  }
+
+  def setoffset(o: Double): Unit = {
+    state += ("b" -> o)
+  }
+
+  override def evaluate(x: DenseVector[Double], y: DenseVector[Double]): Double =
+    math.asin(
+      state("w")*(x.t*y) + state("b")/
+      (math.sqrt(state("w")*(x.t*x) + state("b") + 1) * math.sqrt(state("w")*(y.t*y) + state("b") + 1)))
+
+}
