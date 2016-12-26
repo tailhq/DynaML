2841a2da5cd55d03c2ec3d1429e604be5d678a43
mandar2812 <mandar2812@gmail.com>
2016-12-26 16:50:06 +0100

    Extended Measurable Function representations
    Added:
    
    1. MeasurableDistrRV class

--- dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/analysis/PushforwardMap.scala
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/analysis/PushforwardMap.scala
@@ -1,9 +1,25 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+* */
 package io.github.mandar2812.dynaml.analysis
 
-import breeze.numerics.log
-import breeze.stats.distributions.ContinuousDistr
 import io.github.mandar2812.dynaml.pipes.{DataPipe, Encoder}
-import io.github.mandar2812.dynaml.probability.{ContinuousDistrRV, RandomVarWithDistr, RandomVariable}
+import io.github.mandar2812.dynaml.probability.{ContinuousDistrRV, MeasurableDistrRV}
 import spire.algebra.Field
 
 /**
@@ -18,21 +34,20 @@ abstract class PushforwardMap[
 @specialized(Double) Jacobian](
   implicit detImpl: DataPipe[Jacobian, Double], field: Field[Destination])
   extends Encoder[Source, Destination] { self =>
+
   /**
     * Represents the decoding/inverse operation.
     */
   override val i: DifferentiableMap[Destination, Source, Jacobian]
 
-  def ->[R <: ContinuousDistrRV[Source]](r: R)
-  : RandomVarWithDistr[Destination, ContinuousDistr[Destination]] =
-    RandomVariable(new ContinuousDistr[Destination] {
-      override def unnormalizedLogPdf(x: Destination) =
-        r.underlyingDist.unnormalizedLogPdf(i(x)) + log(detImpl(i.J(x)))
+  def ->[R <: ContinuousDistrRV[Source]](r: R): MeasurableDistrRV[Source, Destination, Jacobian] =
+    new MeasurableDistrRV[Source, Destination, Jacobian](r)(self)
 
-      override def logNormalizer = r.underlyingDist.logNormalizer
-
-      override def draw() = self.run(r.underlyingDist.draw())
-    })
+  /**
+    * Return the implementation used for calculating
+    * determinant of the inverse function's Jacobian
+    * */
+  def _det = detImpl
 
 }
 
@@ -40,6 +55,7 @@ object PushforwardMap {
   def apply[S, D, J](forward: DataPipe[S, D], reverse: DifferentiableMap[D, S, J])(
     implicit detImpl: DataPipe[J, Double], field: Field[D]) =
     new PushforwardMap[S, D, J] {
+
       /**
         * Represents the decoding/inverse operation.
         */
