1bdab4da5c96ae3776df24928b1c458356a5a3d1
mandar2812 <mandar2812@gmail.com>
2017-01-21 12:53:55 +0100

    Added model prediction pipeline

--- dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/models/ModelPipe.scala
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/models/ModelPipe.scala
@@ -12,3 +12,20 @@ trait ModelPipe[Source, T, Q, R, M <: Model[T, Q, R]]
 
   override def run(data: Source): M
 }
+
+/**
+  * A pipeline which encapsulates a DynaML [[Model]]
+  * */
+trait ModelPredictionPipe[T, Q, R, M <: Model[T, Q, R]]
+  extends DataPipe[Q, R] {
+
+  val baseModel: M
+
+  override def run(data: Q) = baseModel.predict(data)
+}
+
+object ModelPredictionPipe {
+  def apply[T, Q, R, M <: Model[T, Q, R]](model: M) = new ModelPredictionPipe[T, Q, R, M] {
+    override val baseModel = model
+  }
+}
