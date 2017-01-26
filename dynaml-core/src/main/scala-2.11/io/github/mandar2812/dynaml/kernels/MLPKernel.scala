84f9d75326969dc675bc04f63ad0a538cc737e90
mandar2812 <mandar2812@gmail.com>
2017-01-26 14:04:12 +0100

    Added gradient implementation to MLP Kernel

--- dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/kernels/MLPKernel.scala
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/kernels/MLPKernel.scala
@@ -1,3 +1,22 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+* */
+
 package io.github.mandar2812.dynaml.kernels
 
 import breeze.linalg.{DenseMatrix, DenseVector}
@@ -28,6 +47,23 @@ class MLPKernel(w: Double, b: Double) extends SVMKernel[DenseMatrix[Double]]
       (state("w")*(x.t*y) + state("b"))/
       (math.sqrt(state("w")*(x.t*x) + state("b") + 1) * math.sqrt(state("w")*(y.t*y) + state("b") + 1)))
 
-  //TODO: Write implementation of kernel gradient.
+  override def gradient(x: DenseVector[Double], y: DenseVector[Double]) = {
+    val (wxy, wxx, wyy) = (
+      state("w")*(x.t*y) + state("b"),
+      math.sqrt(state("w")*(x.t*x) + state("b") + 1),
+      math.sqrt(state("w")*(y.t*y) + state("b") + 1))
 
+    val (numerator, denominator) = (wxy, wxx*wyy)
+
+    val z = numerator/denominator
+
+    val alpha = 1.0/(1.0 - z*z)
+
+    Map(
+      "w" ->
+        alpha*((denominator*(x.t*y) - numerator*0.5*(wyy*(x.t*x)/wxx + wxx*(y.t*y)/wyy))/denominator*denominator),
+      "b" ->
+        alpha*((denominator - numerator*0.5*(wyy/wxx + wxx/wyy))/denominator*denominator)
+    )
+  }
 }
