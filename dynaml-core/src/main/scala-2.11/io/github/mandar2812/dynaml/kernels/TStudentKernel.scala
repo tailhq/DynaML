1fe46faef7cd1e641ff86c5ff4e978e60b5a6aa8
mandar2812 <mandar2812@gmail.com>
2017-01-26 12:40:06 +0100

    Added gradient calculation for TStudent Kernel

--- dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/kernels/TStudentKernel.scala
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/kernels/TStudentKernel.scala
@@ -16,6 +16,7 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 * */
+
 package io.github.mandar2812.dynaml.kernels
 
 import breeze.linalg.{DenseMatrix, DenseVector, norm}
@@ -40,7 +41,11 @@ class TStudentKernel(private var d: Double = 1.0)(implicit ev: Field[DenseVector
 
   def getD: Double = state("d")
 
-  //TODO: Write implementation of kernel gradient.
+  override def gradient(x: DenseVector[Double], y: DenseVector[Double]) = {
+    val dist = norm(x-y)
+    val dist_d = math.pow(dist, state("d"))
+    if(dist > 0.0) Map("d" -> -dist_d*math.log(dist)/math.pow(1.0 + dist_d, 2.0)) else Map("d" -> 0.0)
+  }
 
 }
 
@@ -51,6 +56,12 @@ class TStudentCovFunc(private var d: Double) extends LocalSVMKernel[Double] {
 
   override def evaluate(x: Double, y: Double): Double =
     1.0/(1.0 + math.pow(math.abs(x-y), state("d")))
+
+  override def gradient(x: Double, y: Double) = {
+    val dist = math.abs(x-y)
+    val dist_d = math.pow(dist, state("d"))
+    if(dist > 0.0) Map("d" -> -dist_d*math.log(dist)/math.pow(1.0 + dist_d, 2.0)) else Map("d" -> 0.0)
+  }
 }
 
 class CoRegTStudentKernel(bandwidth: Double) extends LocalSVMKernel[Int] {
@@ -62,6 +73,12 @@ class CoRegTStudentKernel(bandwidth: Double) extends LocalSVMKernel[Int] {
   override def evaluate(x: Int, y: Int): Double = {
     val diff = x - y
     1.0/(1.0 + math.pow(math.abs(diff.toDouble), state("CoRegD")))
+  }
+
+  override def gradient(x: Int, y: Int) = {
+    val dist = math.abs(x-y).toDouble
+    val dist_d = math.pow(dist, state("CoRegD"))
+    if(dist > 0.0) Map("CoRegD" -> -dist_d*math.log(dist)/math.pow(1.0 + dist_d, 2.0)) else Map("CoRegD" -> 0.0)
   }
 
   def getD: Double = state("CoRegD")
