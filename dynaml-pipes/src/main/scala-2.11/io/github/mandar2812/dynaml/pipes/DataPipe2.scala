9c85db2e18cd90e90da8859607213d9aa11da12e
mandar2812 <mandar2812@gmail.com>
2017-02-16 17:40:16 +0100

    Added DataPipe3

--- dynaml-pipes/src/main/scala-2.11/io/github/mandar2812/dynaml/pipes/DataPipe2.scala
+++ dynaml-pipes/src/main/scala-2.11/io/github/mandar2812/dynaml/pipes/DataPipe2.scala
@@ -23,3 +23,24 @@ object DataPipe2 {
       override def run(data1: Source1, data2: Source2) = func2(data1, data2)
     }
 }
+
+
+trait DataPipe3[-Source1, -Source2, -Source3, +Result] extends Serializable {
+  self =>
+
+  def run(data1: Source1, data2: Source2, data3: Source3): Result
+
+  def apply(data1: Source1, data2: Source2, data3: Source3): Result = run(data1, data2, data3)
+
+  def >[Result2](otherPipe: DataPipe[Result, Result2]): DataPipe3[Source1, Source2, Source3, Result2] =
+    DataPipe3((d1: Source1, d2:Source2, d3: Source3) => otherPipe.run(self.run(d1, d2, d3)))
+
+}
+
+object DataPipe3 {
+  def apply[Source1, Source2, Source3, Result](func3: (Source1, Source2, Source3) => Result)
+  : DataPipe3[Source1, Source2, Source3, Result] =
+    new DataPipe3[Source1, Source2, Source3, Result] {
+      override def run(data1: Source1, data2: Source2, data3: Source3) = func3(data1, data2, data3)
+    }
+}
