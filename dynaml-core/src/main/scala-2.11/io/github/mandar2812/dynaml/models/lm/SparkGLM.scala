f6aca63b79c45af41da1309c9300ee735b817713
mandar2812 <mandar2812@gmail.com>
2017-02-17 16:27:17 +0100

    Added licence content to Spark Logistic models

--- dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/models/lm/SparkGLM.scala
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/models/lm/SparkGLM.scala
@@ -16,7 +16,6 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 * */
-
 package io.github.mandar2812.dynaml.models.lm
 
 import breeze.linalg.{DenseMatrix, DenseVector}
