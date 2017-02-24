8de509ab309a576ab91ac99cc9e7ee64c2805031
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2017-02-24 17:53:57 +0530

    Create Main.scala
    command line arguments to script

---
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/probability/Main.scala
@@ -0,0 +1,24 @@
+package io.github.mandar2812.dynaml.probability
+
+import java.io.File
+
+import ammonite.ops.Path
+import ammonite.util.Res.{Exception, Failure}
+
+object Main extends App {
+
+  ammonite.Main(predef = s"import ${getClass.getPackage.getName}._\nval args = ${args.tail.map(escape).mkString("Array(", ", ", ")")}", verboseOutput = false).runScript(Path(new File(args(0)).getAbsolutePath), Seq.empty, Seq.empty) match {
+    case Failure(ex, msg) =>
+      ex match {
+        case Some(t) => throw t
+        case None => throw new RuntimeException(msg)
+      }
+    case Exception(t, msg) => throw t
+    case _ =>
+  }
+
+  def escape(raw: String): String = {
+    import scala.reflect.runtime.universe._
+    Literal(Constant(raw)).toString
+  }
+
