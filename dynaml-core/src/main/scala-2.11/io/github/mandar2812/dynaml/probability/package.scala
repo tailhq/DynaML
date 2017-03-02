7966b19062a3c4d373cf68578fde94732fd604a0
mandar2812 <mandar2812@gmail.com>
2017-03-02 17:17:40 +0100

    Added function to calculate order statistics of a univariate random variable backed by a breeze distribution

--- dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/probability/package.scala
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/probability/package.scala
@@ -3,6 +3,8 @@ package io.github.mandar2812.dynaml
 import scalaxy.streams.optimize
 import spire.algebra.InnerProductSpace
 
+import scala.util.Random
+
 /**
   * Created by mandar on 11/01/2017.
   */
@@ -26,4 +28,31 @@ package object probability {
   }
 
 
+  /**
+    * Calculate the (monte carlo approximation to) mean, median, mode, lower and upper confidence interval.
+    *
+    *
+    *
+    * @param r Continuous random variable in question
+    * @param alpha Probability of exclusion, i.e. return 100(1-alpha) % confidence interval.
+    *              alpha should be between 0 and 1
+    * */
+  def OrderStats(r: ContinuousDistrRV[Double], alpha: Double): (Double, Double, Double, Double, Double) = {
+
+    val samples = r.iid(candidates).sample()
+
+    val mean = samples.sum/candidates.toDouble
+
+    val median = utils.median(samples)
+
+    val (lowerbar, higherbar) = (
+      utils.quickselect(samples, math.ceil(candidates*alpha*0.5).toInt),
+      utils.quickselect(samples, math.ceil(candidates*(1.0 - 0.5*alpha)).toInt))
+
+    val uDist = r.underlyingDist
+
+    val mode = samples.map(s => (uDist.logPdf(s), s)).max._2
+
+    (mean, median, mode, lowerbar, higherbar)
+  }
 }
