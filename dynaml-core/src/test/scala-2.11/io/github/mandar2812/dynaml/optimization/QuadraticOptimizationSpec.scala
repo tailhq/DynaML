f5cf9310f7a8c08a3e64b35a06a73210ecc80076
mandar2812 <mandar2812@gmail.com>
2017-03-24 00:50:09 +0100

    Added extra back propagation test: [WIP]

--- dynaml-core/src/test/scala-2.11/io/github/mandar2812/dynaml/optimization/QuadraticOptimizationSpec.scala
+++ dynaml-core/src/test/scala-2.11/io/github/mandar2812/dynaml/optimization/QuadraticOptimizationSpec.scala
@@ -5,6 +5,8 @@ import breeze.numerics.sigmoid
 import breeze.stats.distributions.{Gaussian, Uniform}
 import io.github.mandar2812.dynaml.DynaMLPipe
 import io.github.mandar2812.dynaml.graph.FFNeuralGraph
+import io.github.mandar2812.dynaml.models.neuralnets._
+import io.github.mandar2812.dynaml.optimization._
 import io.github.mandar2812.dynaml.pipes.DataPipe
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -132,4 +134,55 @@ class QuadraticOptimizationSpec extends FlatSpec with Matchers {
     assert(result._1.length == 3 && result._2.length == 2)
     assert(result._1.head == List(1.0, 4.0) && result._2.head == List(1.0, 2.0))
   }
+
+
+  ignore should "be able to minimize Quadratic cost functions "+
+    "of the form w^t.w + (w.x-y)^2 " in {
+    //Create synthetic data set of x,y values
+    //x is sampled in unit hypercube, y = w.x + noise
+    val noise = new Gaussian(0.0, 0.002)
+    val uniH = new Uniform(-1.0, 1.0)
+
+    val w = DenseVector.tabulate[Double](2)(i => uniH.draw)
+    val wAug = DenseVector(w.toArray ++ Array(0.0))
+    val numPoints:Int = 2000
+
+    val data = (1 to numPoints).map(_ => {
+      val features = DenseVector.tabulate[Double](2)(_ => uniH.draw)
+      val augFeatures = DenseVector(features.toArray ++ Array(1.0))
+
+      val target = (wAug dot augFeatures) + noise.draw
+
+      (features, target)
+    })
+
+    val epsilon = 1E-2
+
+    val transform = DataPipe((s: IndexedSeq[(DenseVector[Double], Double)]) =>
+      s.map(p => (p._1, DenseVector(p._2))).toStream)
+
+    val stackfactory = NeuralStackFactory(Seq(2, 1))(Seq(VectorLinear))
+    val initial_net = stackfactory(Seq((DenseMatrix((uniH.draw(), uniH.draw())), DenseVector(uniH.draw()))))
+
+    val backprop = new FFBackProp(stackfactory).setNumIterations(400).setStepSize(0.9)
+
+    val new_net = backprop.optimize(numPoints, transform(data), initial_net)
+
+    val learned_params = new_net._layers.head.parameters
+    val wApprox = DenseVector(learned_params._1.toDenseVector.toArray ++ learned_params._2.toArray)
+    /*val wApprox = BackPropagation.run(
+      numPoints.toLong, 0.0, 300,
+      1.0, 0.9, 0.5,
+      FFNeuralGraph(
+        2,1,0, List("linear"),
+        List(), biasFlag = true),
+      data, transform)*/
+
+    val err = wApprox - wAug
+    //println("Hidden Layer weights: "+wAug)
+    //println("Calculated Hidden Layer weights: "+wApprox.getSynapsesAsMatrix(1).toDenseVector)
+    //println("Error in Hidden Layer weights: "+err)
+    assert(norm(err) <= epsilon)
+  }
+
 }
