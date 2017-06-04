b5a1ad75c6ceaf28d93e8b9f7623016f89b52dd4
mandar2812 <mandar2812@gmail.com>
2017-06-04 14:39:47 +0200

    Refactoring GP Delve example

--- dynaml-examples/src/main/scala-2.11/io/github/mandar2812/dynaml/examples/TestGPDelve.scala
+++ dynaml-examples/src/main/scala-2.11/io/github/mandar2812/dynaml/examples/TestGPDelve.scala
@@ -7,11 +7,20 @@ import io.github.mandar2812.dynaml.kernels._
 import io.github.mandar2812.dynaml.models.gp.GPRegression
 import io.github.mandar2812.dynaml.pipes.{DataPipe, StreamDataPipe}
 import io.github.mandar2812.dynaml.utils
+import io.github.mandar2812.dynaml.DynaMLPipe._
+import io.github.mandar2812.dynaml.modelpipe.GPRegressionPipe
+import io.github.mandar2812.dynaml.utils.GaussianScaler
 
 /**
   * Example: Gaussian Process regression on fried delve data.
   */
 object TestGPDelve {
+
+  type Features = DenseVector[Double]
+  type Data = Stream[(Features, Features)]
+  type Scales = (GaussianScaler, GaussianScaler)
+  type DataAndScales = (Data, Data, Scales)
+
   def apply (kern: String = "RBF",
              bandwidth: Double = 0.5, noise: Double = 0.0,
              training: Int = 100, test: Int = 1000,
@@ -39,68 +48,43 @@ object TestGPDelve {
     //pipe training data to model and then generate test predictions
     //create RegressionMetrics instance and produce plots
 
-    val extractTrainingFeatures = (l: Stream[String]) =>
-      utils.extractColumns(l, ",", columns, Map())
-
-    val normalizeData =
-      (trainTest: (Stream[(DenseVector[Double], Double)], Stream[(DenseVector[Double], Double)])) => {
-
-        val (mean, variance) = utils.getStats(trainTest._1.map(tup =>
-          DenseVector(tup._1.toArray ++ Array(tup._2))).toList)
-
-        val stdDev: DenseVector[Double] = variance.map(v =>
-          math.sqrt(v/(trainTest._1.length.toDouble - 1.0)))
+    val preScaling = StreamDataPipe((pattern: (DenseVector[Double], Double)) => (pattern._1, DenseVector(pattern._2)))
+    val postScaling = StreamDataPipe((pattern: (Features, Features)) => (pattern._1, pattern._2(0)))
 
 
-        val normalizationFunc = (point: (DenseVector[Double], Double)) => {
-          val extendedpoint = DenseVector(point._1.toArray ++ Array(point._2))
+    val modelPipe = GPRegressionPipe(
+      (d: Data) => d.map(p => (p._1, p._2(0))).toSeq,
+      kernel, new DiracKernel(noise))
 
-          val normPoint = (extendedpoint - mean) :/ stdDev
-          val length = normPoint.length
-          (normPoint(0 until length), normPoint(-1))
-        }
-
-        ((trainTest._1.map(normalizationFunc),
-          trainTest._2.map(normalizationFunc)), (mean, stdDev))
-      }
 
     val modelTrainTest =
-      (trainTest: ((Stream[(DenseVector[Double], Double)],
-        Stream[(DenseVector[Double], Double)]),
-        (DenseVector[Double], DenseVector[Double]))) => {
-        val model = new GPRegression(kernel, new DiracKernel(noise), trainTest._1._1.toSeq)
-        val res = model.test(trainTest._1._2)
-        val scoresAndLabelsPipe =
-          DataPipe(
-            (res: Seq[(DenseVector[Double], Double, Double, Double, Double)]) =>
-              res.map(i => (i._3, i._2)).toList) > DataPipe((list: List[(Double, Double)]) =>
-            list.map{l => (l._1*trainTest._2._2(-1) + trainTest._2._1(-1),
-              l._2*trainTest._2._2(-1) + trainTest._2._1(-1))})
+      (trainTest: DataAndScales) => {
+        val (training, test, scales) = trainTest
 
-        val scoresAndLabels = scoresAndLabelsPipe.run(res)
+        val model = modelPipe(training)
+        val res = model.test(postScaling(test)).map(t => (DenseVector(t._3), DenseVector(t._2)))
 
-        val metrics = new RegressionMetrics(scoresAndLabels,
+        val scoresAndLabels = (scales._2.i * scales._2.i)(res)
+
+        val metrics = new RegressionMetrics(
+          scoresAndLabels.map(p => (p._1(0), p._2(0))).toList,
           scoresAndLabels.length)
 
-        //println(scoresAndLabels)
         metrics.print()
-        //metrics.generatePlots()
+        metrics.generatePlots()
+
+        (model, metrics)
       }
 
-    val preProcessPipe = DataPipe(utils.textFileToStream _) >
-      DataPipe(extractTrainingFeatures) >
-      StreamDataPipe((line: String) => {
-        val split = line.split(",")
-        (DenseVector(split.tail.map(_.toDouble)), split.head.toDouble)
-      })
+    val preProcessPipe = fileToStream >
+      extractTrainingFeatures(columns, Map()) >
+      splitFeaturesAndTargets >
+      preScaling
+
 
     val trainTestPipe = DataPipe(preProcessPipe, preProcessPipe) >
-      DataPipe((data: (Stream[(DenseVector[Double], Double)],
-        Stream[(DenseVector[Double], Double)])) => {
-        (data._1.take(training),
-          data._2.takeRight(test))
-      }) >
-      DataPipe(normalizeData) >
+      splitTrainingTest(training, test) >
+      gaussianScalingTrainTest >
       DataPipe(modelTrainTest)
 
     trainTestPipe.run(("data/delve.csv", "data/delve.csv"))
