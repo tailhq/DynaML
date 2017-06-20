ee43d00f264012bd5dd2964a249c6f97c1d46a41
mandar2812 <mandar2812@gmail.com>
2017-06-20 18:19:27 +0200

    Improvements to install script

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 chmod +x ./target/universal/stage/bin/dynaml
 sudo rm /usr/local/bin/dynaml
 sudo ln -s ${DIR}/target/universal/stage/bin/dynaml /usr/local/bin/dynaml
+sed -i.bak '/export DYNAML_HOME/d' ~/.bash_profile
 echo 'export DYNAML_HOME='${DIR} >>~/.bash_profile
 source ${HOME}/.bash_profile
