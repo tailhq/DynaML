74fde975205198a12275acf7358b07117b77a010
mandar2812 <mandar2812@gmail.com>
2017-08-31 19:27:19 +0200

    Added implicit implementation for Inner Product Space on Tuple2

--- dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/analysis/implicits.scala
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/analysis/implicits.scala
@@ -7,7 +7,8 @@ import spire.algebra.{Field, InnerProductSpace}
   */
 object implicits {
 
-  implicit val innerProdDouble = new InnerProductSpace[Double, Double] {
+  implicit object innerProdDouble extends InnerProductSpace[Double, Double] {
+
     override def dot(v: Double, w: Double) = v*w
 
     override implicit def scalar = Field[Double]
@@ -19,6 +20,21 @@ object implicits {
     override def zero = 0.0
 
     override def plus(x: Double, y: Double) = x + y
+  }
+
+  implicit object innerProdTuple2 extends InnerProductSpace[(Double, Double), Double] {
+    
+    override def dot(v: (Double, Double), w: (Double, Double)): Double = v._1*w._1 + v._2*w._2
+
+    override implicit def scalar: Field[Double] = Field[Double]
+
+    override def timesl(r: Double, v: (Double, Double)): (Double, Double) = (r*v._1, r*v._2)
+
+    override def negate(x: (Double, Double)): (Double, Double) = (-x._1, -x._2)
+
+    override def zero: (Double, Double) = (0d, 0d)
+
+    override def plus(x: (Double, Double), y: (Double, Double)): (Double, Double) = (x._1+y._1, x._2+y._2)
   }
 
 }
