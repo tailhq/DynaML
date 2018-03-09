429b06fa4efd52f5885c90302ab8545c25e87811
mandar2812 <mandar2812@gmail.com>
2018-03-09 15:35:14 +0100

    dynaml.tensorflow: Added Dynamic System simulation and inference
     - FiniteHorizonCTRNN: A continuous time recurrent neural network layer.
     - Loss for time slices of multivariate time series.

---
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/tensorflow/layers/FiniteHorizonCTRNN.scala
@@ -0,0 +1,61 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+* */
+package io.github.mandar2812.dynaml.tensorflow.layers
+
+import org.platanios.tensorflow.api.{Output, Shape, tf}
+import org.platanios.tensorflow.api.learn.Mode
+import org.platanios.tensorflow.api.learn.layers.Layer
+import org.platanios.tensorflow.api.ops.variables.{Initializer, RandomNormalInitializer}
+
+/**
+  * Represents a Continuous Time Recurrent Neural Network (CTRNN)
+  * The layer simulates the discretized dynamics of the CTRNN for
+  * a fixed number of time steps.
+  *
+  * @author mandar2812 date: 2018/03/06
+  * */
+case class FiniteHorizonCTRNN(
+  override val name: String, units: Int,
+  horizon: Int, timestep: Double,
+  weightsInitializer: Initializer = RandomNormalInitializer(),
+  biasInitializer: Initializer = RandomNormalInitializer(),
+  gainInitializer: Initializer = RandomNormalInitializer(),
+  timeConstantInitializer: Initializer = RandomNormalInitializer()) extends
+  Layer[Output, Output](name) {
+
+  override val layerType: String = "FHCTRNN"
+
+  override protected def _forward(input: Output, mode: Mode): Output = {
+
+    val weights      = tf.variable("Weights", input.dataType, Shape(units, units), weightsInitializer)
+    val timeconstant = tf.variable("TimeConstant", input.dataType, Shape(units, units), timeConstantInitializer)
+    val gain         = tf.variable("Gain", input.dataType, Shape(units, units), timeConstantInitializer)
+    val bias         = tf.variable("Bias", input.dataType, Shape(units), biasInitializer)
+
+    tf.stack(
+      (1 to horizon).scanLeft(input)((x, _) => {
+        val decay = x.tensorDot(timeconstant.multiply(-1d), Seq(1), Seq(0))
+        val interaction = x.tensorDot(gain, Seq(1), Seq(0)).add(bias).tanh.tensorDot(weights, Seq(1), Seq(0))
+
+        x.add(decay.multiply(timestep)).add(interaction.multiply(timestep))
+      }).tail,
+      axis = -1)
+
+  }
+}
---
+++ dynaml-core/src/main/scala-2.11/io/github/mandar2812/dynaml/tensorflow/learn/MVTimeSeriesLoss.scala
@@ -0,0 +1,37 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+* */
+package io.github.mandar2812.dynaml.tensorflow.learn
+
+import org.platanios.tensorflow.api.learn.Mode
+import org.platanios.tensorflow.api.learn.layers.Loss
+import org.platanios.tensorflow.api.ops.Output
+
+/**
+  * L2 loss for a time slice of a multivariate time series
+  *
+  * @author mandar2812 date 9/03/2018
+  * */
+case class MVTimeSeriesLoss(override val name: String)
+  extends Loss[(Output, Output)](name) {
+  override val layerType: String = "L2Loss"
+
+  override protected def _forward(input: (Output, Output), mode: Mode): Output = {
+    input._1.subtract(input._2).square.mean(axes = 0).sum()
+  }
+}
