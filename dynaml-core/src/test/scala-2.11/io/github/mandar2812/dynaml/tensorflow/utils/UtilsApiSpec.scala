d297bcd16197584889af6c8b59c4a382b1e5fd15
Mandar Chandorkar <mandar2812@gmail.com>
2018-09-10 20:24:46 +0200

    [dynaml-tensorflow]:   -- Added tests for tensorflow `utils` package
    Signed-off-by: mandar2812 <mandar2812@gmail.com>

---
+++ dynaml-core/src/test/scala-2.11/io/github/mandar2812/dynaml/tensorflow/utils/UtilsApiSpec.scala
@@ -0,0 +1,40 @@
+package io.github.mandar2812.dynaml.tensorflow.utils
+
+import org.scalatest.{FlatSpec, Matchers}
+import io.github.mandar2812.dynaml.tensorflow._
+import org.platanios.tensorflow.api._
+import _root_.io.github.mandar2812.dynaml.probability.GaussianRV
+
+
+class UtilsApiSpec extends FlatSpec with Matchers {
+
+  "Regression Metrics on Tensors" should " compute the relevant metrics correctly" in {
+
+    val t = dtf.random(FLOAT64, 10)(GaussianRV(1.0, 1.0))
+
+    val metricsTF = new GenRegressionMetricsTF(t, t)
+
+    assert(
+      tfi.abs(
+        metricsTF.results - dtf.tensor_f64(4)(0d, 0d, 1d, 1d)
+      ).mean().scalar.asInstanceOf[Double] < 1E-7)
+
+  }
+
+  "Gaussian and Min-Max scaling " should "compute correctly on tensors" in {
+
+    val data = dtf.tensor_f64(2)(0d, 1d)
+
+    val sc_g = dtfpipe.gauss_std()(data)
+    val sc_m = dtfpipe.minmax_std()(data)
+
+    assert(
+      sc_g._2.mean.reshape(Shape(1)) == Tensor(0.5) &&
+        tfi.abs(sc_g._2.sigma.square.reshape(Shape(1)) - Tensor(0.5)).scalar.asInstanceOf[Double] < 1E-6)
+
+    assert(
+      sc_m._2.min.reshape(Shape(1)) == Tensor(0d) &&
+        sc_m._2.max.reshape(Shape(1)) == Tensor(1d))
+  }
+
+}
