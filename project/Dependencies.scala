efe279695eb2b373e95442062fec0a0106cb9a3d
Scala Steward <me@scala-steward.org>
2019-08-28 15:03:51 +0200

    Update ammonite-repl, ammonite-sshd to 1.6.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -126,8 +126,8 @@ object Dependencies {
   )
 
   val ammoniteDeps = Seq(
-    "com.lihaoyi" %% "ammonite-repl" % "1.6.9-15-6720d42" cross CrossVersion.full,
-    "com.lihaoyi" %% "ammonite-sshd" % "1.6.9-15-6720d42" cross CrossVersion.full
+    "com.lihaoyi" %% "ammonite-repl" % "1.6.9" cross CrossVersion.full,
+    "com.lihaoyi" %% "ammonite-sshd" % "1.6.9" cross CrossVersion.full
   )
 
   val commons_io = Seq("commons-io" % "commons-io" % "2.6")
