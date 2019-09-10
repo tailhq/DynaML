e6f10a50c54371260176640a3213bdcc6f8ac0e1
mandar2812 <mandar2812@gmail.com>
2019-09-10 15:10:42 +0200

    Updated install script

--- sbt-shell.sh
+++ sbt-shell.sh
@@ -6,4 +6,6 @@ gpuFlag=${2:-false}
 
 heapSize=${1:-4096m}
 
+echo "Starting sbt shell with Build Executable Heap Size = $heapSize and GPU Flag = $gpuFlag"
+
 sbt -Dheap=${heapSize} -Dgpu=${gpuFlag} -DpackagedTF=${pTF}
