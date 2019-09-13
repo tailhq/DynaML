b942af9824deac6cc98a35aca1063c241c20c30c
Mandar Chandorkar <mandar2812@gmail.com>
2019-09-13 22:51:32 +0200

    [dynaml-core]: Improvements to monte carlo quadrature rule. [dynaml-tensorflow]: Fix to random tensor generation.

--- dynaml-tensorflow/src/main/scala/io/github/mandar2812/dynaml/tensorflow/api/Api.scala
+++ dynaml-tensorflow/src/main/scala/io/github/mandar2812/dynaml/tensorflow/api/Api.scala
@@ -293,7 +293,7 @@ private[tensorflow] object Api {
     * [[RandomVariable]] instance.
     * */
   def random[D: TF](shape: Int*)(rv: RandomVariable[D]): Tensor[D] = {
-    val buffer = rv.iid(shape.product).draw
+    val buffer = rv.iid(shape.product).draw.toSeq
     Tensor[D](buffer).reshape(Shape(shape: _*))
   }
 
