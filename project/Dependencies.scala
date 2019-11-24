c9c5463b3b438dca5c39f3fbac30f86788100e65
Scala Steward <me@scala-steward.org>
2019-11-24 14:28:15 +0100

    Update case-app to 2.0.0-M9+34-930e7901-SNAPSHOT

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -203,7 +203,7 @@ object Dependencies {
     "sh.almond"                  %% "scala-interpreter" % "0.8.2" cross CrossVersion.full,
     "sh.almond"                  %% "scala-kernel-api"  % "0.8.2" cross CrossVersion.full,
     "sh.almond"                  %% "kernel"            % "0.8.2",
-    "com.github.alexarchambault" %% "case-app"          % "2.0.0-M9+32-cf2d0d91-SNAPSHOT"
+    "com.github.alexarchambault" %% "case-app"          % "2.0.0-M9+34-930e7901-SNAPSHOT"
   )
 
   val pipesDependencies = (
