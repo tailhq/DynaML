1d590ba97d95f91fb5f07064f534b8af33ac9ac4
Scala Steward <me@scala-steward.org>
2020-01-27 17:05:02 +0100

    Update akka-actor, akka-stream, ... to 2.6.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -158,9 +158,9 @@ object Dependencies {
   )
 
   val dynaServeDependencies = Seq(
-    "com.typesafe.akka" %% "akka-actor"           % "2.6.1",
-    "com.typesafe.akka" %% "akka-stream"          % "2.6.1",
-    "com.typesafe.akka" %% "akka-testkit"         % "2.6.1",
+    "com.typesafe.akka" %% "akka-actor"           % "2.6.2",
+    "com.typesafe.akka" %% "akka-stream"          % "2.6.2",
+    "com.typesafe.akka" %% "akka-testkit"         % "2.6.2",
     "com.typesafe.akka" %% "akka-http"            % "10.1.11",
     "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.11",
     "com.typesafe.akka" %% "akka-http-testkit"    % "10.1.11"
