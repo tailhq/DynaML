e2443bce28cc52d7093206233975bd7c55f88ebe
Scala Steward <me@scala-steward.org>
2020-04-15 22:20:43 +0200

    Update coursier to 2.0.0-RC6-12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -195,7 +195,7 @@ object Dependencies {
   )
 
   val coursier_deps = Seq(
-    "io.get-coursier" %% "coursier" % "2.0.0-RC6-11",
+    "io.get-coursier" %% "coursier" % "2.0.0-RC6-12",
     "io.get-coursier" % "interface" % "0.0.21"
   )
 
