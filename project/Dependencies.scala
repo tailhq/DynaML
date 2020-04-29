22d158fc326200d5fef26f28a3fb9666e98d1cd5
Scala Steward <me@scala-steward.org>
2020-04-29 17:10:57 +0200

    Update jackson-module-scala to 2.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -108,7 +108,7 @@ object Dependencies {
     "org.apache.spark"             %% "spark-core"           % "2.4.4",
     "org.apache.spark"             %% "spark-mllib"          % "2.4.4",
     "com.fasterxml.jackson.core"   % "jackson-databind"      % "2.10.3",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.10.3"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.11.0"
   ).map(
     _.withExclusions(
       Vector(
